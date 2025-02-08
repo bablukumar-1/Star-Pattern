fb2cd0117d56d933b2e4583d69b00442549e3038
bablukumar <bablu824130@gmail.com>
2025-02-08 14:29:03 +0530

    Caommited basic stars pattern

---
+++ Trangular.js
@@ -0,0 +1,43 @@
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=i; j++){
+//     process.stdout.write('*');  
+//     }
+//     console.log('')
+// }
+// output
+
+// *
+// **
+// ***
+// ****
+// *****
+
+
+
+// for(let i=8; i>=1; i--){
+//     for(let j=1; j<=i; j++){
+//     process.stdout.write('*');  
+//     }
+//     console.log('')
+// }
+
+//output
+
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+
+
+for(let i=1; i<=10; i++){
+    for(let j=1; j<=i; j+1){
+    process.stdout.write('*');  
+    }
+    console.log('')
+}
+
